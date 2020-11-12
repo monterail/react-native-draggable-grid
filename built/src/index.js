71a1722bc666f843f39ce733b79bbca6882e4f3f
Alexandru Mic <alexandru.mic07@gmail.com>
2020-11-12 12:17:02 +0200

    add built folder for now

---
+++ built/src/index.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var draggable_grid_1 = require("./draggable-grid");
+exports.DraggableGrid = draggable_grid_1.DraggableGrid;
+exports.default = draggable_grid_1.DraggableGrid;
+//# sourceMappingURL=index.js.map
